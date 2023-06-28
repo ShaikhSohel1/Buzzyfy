ed15b496ab850335e7ba3e2a0f8c8a13b21a9cf5
ShaikhSohel <sh1052887120@gmail.com>
2023-06-28 21:33:57 +0530

    Supabase Setup

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import {FC} from "react";
+import { FaPlay } from "react-icons/fa";
 
 
 interface ListItemProps {
@@ -24,7 +25,9 @@ const ListItem:FC<ListItemProps> = ({
     }
 
     return ( 
-        <button className=" relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
+        <button 
+        onClick={onClick}
+        className=" relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
                 <div className="relative min-h-[64px] min-w-[64px]">
                     <Image  
                     className="object-cover"
@@ -33,7 +36,11 @@ const ListItem:FC<ListItemProps> = ({
                     alt="image"/>
 
                 </div>
-                <p>{name}</p>
+                <p className="font-medium truncate py-5"
+                >{name}</p>
+                <div className="absolute transition opacity-0 rounded-full flex items-center justify-center bg-green-500 p-4 drop-shadow-md right-5 group-hover:opacity-100 hover:scale-110 ">
+                <FaPlay className="text-black"/>
+                </div>
         </button>
      );
 }
